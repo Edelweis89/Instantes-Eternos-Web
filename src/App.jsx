b735fdfa34003eb7a58def67c6e47ca007588691
Nicolas Avila <nicolas.avila09@outlook.com>
2024-09-07 19:55:33 -0300

    repasando usestate

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './app.css'
 import Footer from './assets/componentes/Footer/Footer'
 import { BrowserRouter } from 'react-router-dom';
 import Card from './assets/componentes/Cards/Card';
+import Nosotros from './assets/componentes/Nosotros/Nosotros';
 
 function App() {
   
@@ -30,22 +31,13 @@ function App() {
        
       </section>
       
-      <section className='hacemos'>
-      <h2>Capturando Momentos, Creando Recuerdos</h2>
+      <section >
+  
 
-      <p>En Instantes Eternos, nos especializamos en capturar esos momentos únicos y especiales que merecen ser recordados para siempre. Nuestro estudio fotográfico se dedica a transformar cada instante en una obra de arte, asegurándonos de que cada fotografía cuente una historia y evoque emociones. </p>
-
-  <h3>Nuestros Servicios Incluyen: </h3>
-
-  <li>Fotografía de Bodas: Inmortalizamos el día más importante de tu vida con imágenes llenas de amor y alegría.</li>
-  <li>Sesiones de Retrato: Capturamos la esencia de cada persona, creando retratos que reflejan su verdadera personalidad. </li>
-  <li>Fotografía de Eventos: Desde cumpleaños hasta eventos corporativos, estamos allí para documentar cada detalle.</li>
-  <li>Fotografía de Naturaleza: Nos apasiona capturar la belleza del mundo natural, desde paisajes impresionantes hasta la vida silvestre.</li>
-
+    <Nosotros  />
+  
     
-    <p>Nuestra Filosofía: Creemos que cada fotografía debe ser una ventana a un momento eterno. Nos esforzamos por ofrecer un servicio personalizado y profesional, utilizando la última tecnología y técnicas innovadoras para garantizar que cada imagen sea perfecta. </p>
-    
-<p>Contáctanos: Estamos ubicados en el corazón de Córdoba, y estamos ansiosos por ayudarte a capturar tus momentos más preciados. ¡Contáctanos hoy y descubre cómo podemos hacer que tus recuerdos duren para siempre!</p>
+     <p> Contáctanos: Estamos ubicados en el corazón de Córdoba, y estamos ansiosos por ayudarte a capturar tus momentos más preciados. ¡Contáctanos hoy y descubre cómo podemos hacer que tus recuerdos duren para siempre!</p>
 
       
 
---
+++ src/assets/componentes/Nosotros/Nosotros.jsx
@@ -0,0 +1,29 @@
+import './nosotros.css'
+
+function nosotros (props) {
+    console.log (props);
+    return (
+
+       
+
+        <section className='hacemos'> 
+
+<h2>Capturando Momentos, Creando Recuerdos</h2>
+
+<p>En Instantes Eternos, nos especializamos en capturar esos momentos únicos y especiales que merecen ser recordados para siempre. Nuestro estudio fotográfico se dedica a transformar cada instante en una obra de arte, asegurándonos de que cada fotografía cuente una historia y evoque emociones. </p>
+
+<h3>Nuestros Servicios Incluyen: </h3>
+
+<li>Fotografía de Bodas: Inmortalizamos el día más importante de tu vida con imágenes llenas de amor y alegría.</li>
+<li>Sesiones de Retrato: Capturamos la esencia de cada persona, creando retratos que reflejan su verdadera personalidad. </li>
+<li>Fotografía de Eventos: Desde cumpleaños hasta eventos corporativos, estamos allí para documentar cada detalle.</li>
+<li>Fotografía de Naturaleza: Nos apasiona capturar la belleza del mundo natural, desde paisajes impresionantes hasta la vida silvestre.</li>
+
+             <p>Nuestra Filosofía: Creemos que cada fotografía debe ser una ventana a un momento eterno. Nos esforzamos por ofrecer un servicio personalizado y profesional, utilizando la última tecnología y técnicas innovadoras para garantizar que cada imagen sea perfecta. </p>
+
+        </section>
+
+    )
+        
+    } export default nosotros;
+    
